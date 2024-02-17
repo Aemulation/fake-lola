888429fee91b689c6567ef1f09a982296d65e1c4
Aemulation <112875887+Aemulation@users.noreply.github.com>
2024-02-17 20:48:34 +0100

    Cleanup + don't crash on yggdrasil exit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::io::prelude::*;
 use std::os::unix::net::UnixListener;
+use std::time::Duration;
 
 use rmp_serde::encode;
 use serde::Serialize;
 
 const LOLA_BUFFER_SIZE: usize = 896;
-const LOLA_PATH: &'static str = "/tmp/robocup";
+const LOLA_PATH: &str = "/tmp/robocup";
 
 #[derive(Default, Debug, Serialize)]
 #[serde(rename_all = "PascalCase")]
@@ -26,31 +27,36 @@ struct LolaNaoState<'a> {
     robot_config: [&'a str; 4],
 }
 
+fn run(listener: &UnixListener, write_buf: &mut [u8]) -> std::io::Result<()> {
+    let mut read_buf = Vec::new();
+
+    match listener.accept() {
+        Ok((mut socket, addr)) => {
+            socket.set_read_timeout(Some(Duration::from_millis(1)))?;
+            println!("Got a client: {:?} - {:?}", socket, addr);
+
+            loop {
+                socket.write_all(write_buf)?;
+
+                let _ = socket.read_to_end(&mut read_buf);
+                read_buf.clear();
+            }
+        }
+        Err(error) => println!("accept: {:?}", error),
+    }
+
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
     let _ = std::fs::remove_file(LOLA_PATH);
     let listener = UnixListener::bind(LOLA_PATH)?;
 
     let mut write_buf = [0u8; LOLA_BUFFER_SIZE];
-
     let lola_state = LolaNaoState::default();
     encode::write_named(&mut write_buf.as_mut_slice(), &lola_state).unwrap();
-    // lola_state.encode(write_buf.as_mut_slice()).unwrap();
-    // let write_buf = [0u8; LOLA_BUFFER_SIZE];
-    // let mut read_buf = Vec::new();
 
-    match listener.accept() {
-        Ok((mut socket, addr)) => {
-            println!("Got a client: {:?} - {:?}", socket, addr);
-
-            loop {
-                socket.write_all(&write_buf)?;
-                // eprintln!("wrote");
-
-                // socket.read_to_end(&mut read_buf)?;
-                // eprintln!("read");
-            }
-        }
-        Err(e) => println!("accept function failed: {:?}", e),
+    loop {
+        let _ = run(&listener, &mut write_buf);
     }
-    Ok(())
 }
